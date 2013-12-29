7e35ad4860e57b1798635b02f67ce5c3dc2fd4c6
Ant Zucaro <azucaro@gmail.com>
2013-12-29 18:03:22 -0500

    Tests for NYSIIS.
    This originally used a corpus from the "aspell" dictionary, which
    was run through a small program to generate the corresponding
    NYSIIS output using the apache commons implementation. I felt
    this was overkill for this particular algorithm (unlike that of
    double metaphone which is way more complex), so I replaced it with
    these simpler cases.

---
+++ nysiis_test.go
@@ -0,0 +1,52 @@
+package matchr
+
+import "testing"
+
+var nysiistests = []struct {
+	s1      string
+	nysiis  string
+}{
+    {"knight",  "NAGT"},
+    {"mitchell",  "MATCAL"},
+    {"o'daniel",  "ODANAL"},
+    {"brown sr",  "BRANSR"},
+    {"browne III",  "BRAN"},
+    {"browne IV",  "BRANAV"},
+    {"O'Banion",  "OBANAN"},
+    {"Mclaughlin",  "MCLAGL"},
+    {"McCormack",  "MCARNA"},
+    {"Chapman",  "CAPNAN"},
+    {"Silva",  "SALV"},
+    {"McDonald",  "MCDANA"},
+    {"Lawson",  "LASAN"},
+    {"Jacobs",  "JACAB"},
+    {"Greene",  "GRAN"},
+    {"O'Brien",  "OBRAN"},
+    {"Morrison",  "MARASA"},
+    {"Larson",  "LARSAN"},
+    {"Willis",  "WAL"},
+    {"Mackenzie",  "MCANSY"},
+    {"Carr",  "CAR"},
+    {"Lawrence",  "LARANC"},
+    {"Matthews",  "MAT"},
+    {"Richards",  "RACARD"},
+    {"Bishop",  "BASAP"},
+    {"Franklin",  "FRANCL"},
+    {"McDaniel",  "MCDANA"},
+    {"Harper",  "HARPAR"},
+    {"Lynch",  "LYNC"},
+    {"Watkins",  "WATCAN"},
+    {"Carlson",  "CARLSA"},
+    {"Wheeler",  "WALAR"},
+    {"Louis XVI",  "LASXV"},
+}
+
+// NYSIIS
+func TestNYIIS(t *testing.T) {
+	for _, tt := range nysiistests {
+		nysiis := NYSIIS(tt.s1)
+		if nysiis != tt.nysiis {
+			t.Errorf("NYSIIS('%s') = %v, want %v", tt.s1, nysiis, tt.nysiis)
+		}
+	}
+}
