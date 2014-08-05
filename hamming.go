78ccbcd8af00cae922b3c83151f81e7a0b82a84d
Ant Zucaro <azucaro@gmail.com>
2014-08-04 21:13:09 -0400

    Document Hamming.

--- hamming.go
+++ hamming.go
@@ -2,6 +2,10 @@ package matchr
 
 import "errors"
 
+// Hamming computes the Hamming distance between two equal-length strings.
+// This is the number of times the two strings differ between characters at 
+// the same index. This implementation is based off of the algorithm
+// description found at http://en.wikipedia.org/wiki/Hamming_distance.
 func Hamming(s1 string, s2 string) (distance int, err error) {
     // index by code point, not byte
     r1 := []rune(s1)
